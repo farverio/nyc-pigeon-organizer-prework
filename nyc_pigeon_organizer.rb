131fac853333648a500267095c984d4c335b8563
Stephen F <stephen@farver.io>
2018-06-04 04:19:10 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,12 +3,12 @@ def nyc_pigeon_organizer(data)
   
   data.each do |attribute, details|
     details.each do |detail_name, pigeon_names|
-      puts output_hash
-      if !output_hash[pigeon_names.to_sym]
-        output_hash[pigeon_names.to_sym] = {}
-      end
+      puts output_hash[pigeon_names.to_sym]
+      # if !output_hash[pigeon_names.to_sym]
+      #   output_hash[pigeon_names.to_sym] = {}
+      # end
       
-      output_hash[pigeon_names.to_sym][attribute] = detail_name.to_s
+      # output_hash[pigeon_names.to_sym][attribute] = detail_name.to_s
     end
   end
   # puts output_hash
